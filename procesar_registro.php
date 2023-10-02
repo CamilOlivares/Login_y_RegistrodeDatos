61679bc9c204ce96639464274b3414bf70c240f3
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2023-10-02 01:03:52 -0300

    procesar_registro.php

--- procesar_registro.php
+++ procesar_registro.php
@@ -1,33 +1,30 @@
 <?php
-// Acceso base abreviado a la datos con "require"
-require("database.php");
+// Acceso abreviado a la base de datos con "require"
+require "database.php";
 
 // Resto del código para procesar la solicitud
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Valida los valores de los campos del formulario
-
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $identificacion = $_POST['identificacion'];
-    $direccion = $_POST['direccion'];
-    $telefono = $_POST['telefono'];
-    $correo = $_POST['correo'];
-
-    $marca = $_POST['marca'];
-    $modelo = $_POST['modelo'];
-    $año = $_POST['año'];
-    $tipo_de_direccion = $_POST['tipo_de_direccion'];
-    $falla = $_POST['falla'];
-    $tecnico_asignado = $_POST['tecnico_asignado']; // Asumiendo que tienes un campo "tecnico_asignado" en tu formulario
+    // Validar y obtener los valores de los campos del formulario
+    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+    $apellido = isset($_POST['apellido']) ? $_POST['apellido'] : '';
+    $identificacion = isset($_POST['identificacion']) ? $_POST['identificacion'] : '';
+    $direccion = isset($_POST['direccion']) ? $_POST['direccion'] : '';
+    $telefono = isset($_POST['telefono']) ? $_POST['telefono'] : '';
+    $correo = isset($_POST['correo']) ? $_POST['correo'] : '';
+    $marca = isset($_POST['marca']) ? $_POST['marca'] : '';
+    $modelo = isset($_POST['modelo']) ? $_POST['modelo'] : '';
+    $año = isset($_POST['año']) ? $_POST['año'] : '';
+    $tipo_de_direccion = isset($_POST['tipo_de_direccion']) ? $_POST['tipo_de_direccion'] : '';
+    $falla = isset($_POST['falla']) ? $_POST['falla'] : '';
+    $tecnico_asignado = isset($_POST['tecnico_asignado']) ? $_POST['tecnico_asignado'] : '';
 
     try {
         // Prepara una consulta SQL para insertar datos en la base de datos
         $sql = "INSERT INTO registro_vehiculos (nombre, apellido, identificacion, direccion, telefono, correo, marca, modelo, año, tipo_de_direccion, falla, tecnico_asignado) 
         VALUES ('$nombre', '$apellido', '$identificacion', '$direccion', '$telefono', '$correo', '$marca', '$modelo', '$año', '$tipo_de_direccion', '$falla', '$tecnico_asignado')";
 
-
         // Ejecuta la consulta SQL
-        $conn->exec($sql);
+        $conn->query($sql);
 
         // Redirecciona a una página de éxito después del registro
         header("Location: registro_exitoso.php");
