49443806c2cdffef0599d279950a8e54b8277190
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2023-10-02 00:31:41 -0300

    registro_exitoso.php

---
+++ registro_exitoso.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registro Exitoso</title>
+    <link rel="stylesheet" href="assets\css\style.css">  <!-- Incluye tu archivo CSS aquí -->
+</head>
+<body>
+    <h1>Registro Exitoso</h1>
+    <p>El registro se ha completado exitosamente. ¡Gracias por registrar la recepción del vehículo!</p>
+
+    <!-- Agrega un enlace para volver a la página de inicio o a alguna otra página importante -->
+    <a href="registro_vehiculos.php">Registrar otro Vehiculo</a>
+    <a href="Index.php">Inicio</a>
+    <!-- Botón de logout -->
+    <?php require "partials/cerrar_sesion.php" ?>  <!-- invoco el boton de "cerrar sesion" -->
+</body>
+</html>
